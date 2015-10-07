e68d6caab9169d9ccc85ec9ea1f5673a2caab60e
Florian Köhler <Florian.Koehler@gmx.net>
2015-10-07 11:40:19 +0200

    changes for automatic testing

--- atf_test/scripts/publish_tf.py
+++ atf_test/scripts/publish_tf.py
@@ -3,6 +3,8 @@ import math
 import rospy
 import unittest
 import rostest
+import rostopic
+import yaml
 
 from tf import transformations, TransformListener, TransformBroadcaster
 from atf_recorder import RecordingManager
@@ -19,9 +21,9 @@ class PublishTf:
         self.child2_frame_id = "reference2"
         self.child3_frame_id = "reference3"
         self.child4_frame_id = "reference4"
-        rospy.Timer(rospy.Duration(1/self.pub_freq), self.reference2)
-        rospy.Timer(rospy.Duration(1/self.pub_freq), self.reference3)
-        rospy.Timer(rospy.Duration(1/self.pub_freq), self.reference4)
+        rospy.Timer(rospy.Duration(1 / self.pub_freq), self.reference2)
+        rospy.Timer(rospy.Duration(1 / self.pub_freq), self.reference3)
+        rospy.Timer(rospy.Duration(1 / self.pub_freq), self.reference4)
         rospy.sleep(1.0)
 
         recorder_0 = RecordingManager("all")
@@ -31,13 +33,13 @@ class PublishTf:
 
         recorder_0.start()
         recorder_1.start()
-        self.pub_line(length=-1, time=2.5)
+        self.pub_line(length=-1, time=1)
         recorder_1.stop()
         recorder_2.start()
-        self.pub_quadrat(length=2, time=60)
+        self.pub_quadrat(length=2, time=1)
         recorder_2.stop()
         recorder_3.start()
-        self.pub_circ(radius=2, time=10)
+        self.pub_circ(radius=2, time=1)
         recorder_3.stop()
         recorder_0.stop()
 
@@ -63,23 +65,23 @@ class PublishTf:
         rospy.loginfo("Line")
         rate = rospy.Rate(int(self.pub_freq))
 
-        for i in range((int(self.pub_freq*time/2)+1)):
-            t = i/self.pub_freq/time*2
-            self.pub_tf(self.parent_frame_id, self.child1_frame_id, [t*length, 0, 0])
+        for i in range((int(self.pub_freq * time / 2) + 1)):
+            t = i / self.pub_freq / time * 2
+            self.pub_tf(self.parent_frame_id, self.child1_frame_id, [t * length, 0, 0])
             rate.sleep()
-        for i in range((int(self.pub_freq*time/2)+1)):
-            t = i/self.pub_freq/time*2
-            self.pub_tf(self.parent_frame_id, self.child1_frame_id, [(1-t)*length, 0, 0])
+        for i in range((int(self.pub_freq * time / 2) + 1)):
+            t = i / self.pub_freq / time * 2
+            self.pub_tf(self.parent_frame_id, self.child1_frame_id, [(1 - t) * length, 0, 0])
             rate.sleep()
 
     def pub_circ(self, radius=1, time=1):
         rospy.loginfo("Circ")
         rate = rospy.Rate(int(self.pub_freq))
 
-        for i in range(int(self.pub_freq*time)+1):
-            t = i/self.pub_freq/time
-            self.pub_tf(self.parent_frame_id, self.child1_frame_id, [-radius*math.cos(2*math.pi*t)+radius,
-                                                                     -radius*math.sin(2*math.pi*t),
+        for i in range(int(self.pub_freq * time) + 1):
+            t = i / self.pub_freq / time
+            self.pub_tf(self.parent_frame_id, self.child1_frame_id, [-radius * math.cos(2 * math.pi * t) + radius,
+                                                                     -radius * math.sin(2 * math.pi * t),
                                                                      0])
             rate.sleep()
 
@@ -87,21 +89,21 @@ class PublishTf:
         rospy.loginfo("Quadrat")
         rate = rospy.Rate(int(self.pub_freq))
 
-        for i in range((int(self.pub_freq*time/4)+1)):
-            t = i/self.pub_freq/time*4
-            self.pub_tf(self.parent_frame_id, self.child1_frame_id, [t*length, 0, 0])
+        for i in range((int(self.pub_freq * time / 4) + 1)):
+            t = i / self.pub_freq / time * 4
+            self.pub_tf(self.parent_frame_id, self.child1_frame_id, [t * length, 0, 0])
             rate.sleep()
-        for i in range((int(self.pub_freq*time/4)+1)):
-            t = i/self.pub_freq/time*4
-            self.pub_tf(self.parent_frame_id, self.child1_frame_id, [length, t*length, 0])
+        for i in range((int(self.pub_freq * time / 4) + 1)):
+            t = i / self.pub_freq / time * 4
+            self.pub_tf(self.parent_frame_id, self.child1_frame_id, [length, t * length, 0])
             rate.sleep()
-        for i in range((int(self.pub_freq*time/4)+1)):
-            t = i/self.pub_freq/time*4
-            self.pub_tf(self.parent_frame_id, self.child1_frame_id, [(1-t)*length, length, 0])
+        for i in range((int(self.pub_freq * time / 4) + 1)):
+            t = i / self.pub_freq / time * 4
+            self.pub_tf(self.parent_frame_id, self.child1_frame_id, [(1 - t) * length, length, 0])
             rate.sleep()
-        for i in range((int(self.pub_freq*time/4)+1)):
-            t = i/self.pub_freq/time*4
-            self.pub_tf(self.parent_frame_id, self.child1_frame_id, [0, (1-t)*length, 0])
+        for i in range((int(self.pub_freq * time / 4) + 1)):
+            t = i / self.pub_freq / time * 4
+            self.pub_tf(self.parent_frame_id, self.child1_frame_id, [0, (1 - t) * length, 0])
             rate.sleep()
 
     @staticmethod
@@ -111,11 +113,31 @@ class PublishTf:
 
 
 class TestRecording(unittest.TestCase):
+    def setUp(self):
+        robot_config = self.load_data(rospy.get_param('/robot_config'))
+        self.topics = robot_config['wait_for_topics']
+        self.services = robot_config['wait_for_services']
 
     def test_Recording(self):
+        # Wait for topics and services
+        for topic in self.topics:
+            rospy.wait_for_message(topic, rostopic.get_topic_class(topic, blocking=True)[0], timeout=None)
+
+        for service in self.services:
+            rospy.wait_for_service(service, timeout=None)
 
         PublishTf()
 
+    @staticmethod
+    def load_data(filename):
+        rospy.loginfo("Reading data from yaml file...")
+
+        with open(filename, 'r') as stream:
+            doc = yaml.load(stream)
+
+        return doc
+
+
 if __name__ == '__main__':
-    rospy.init_node('test_recording')
+    rospy.init_node('publish_tf')
     rostest.rosrun("atf_test", 'test_recording', TestRecording, sysargs=None)
